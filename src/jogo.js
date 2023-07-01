2c44f9ded7a9db0d57a1cf9e9a5917ebfbefc108
victordamascenooliveira@gmail.com <victordamascenooliveira@gmail.com>
2023-07-01 17:55:36 -0300

    criacao estrutura para o baralho

---
+++ src/jogo.js
@@ -0,0 +1,39 @@
+function testeprint(a){
+    a.innerText = "gameplay";
+}
+
+class Baralho{
+    constructor() {
+        this.stack = [];
+     }
+     add(carta) {
+        return this.stack.push(carta);
+     }
+     remove() {
+        if (this.stack.length > 0) {
+            stack.pop();
+        }
+     }
+     peek() {
+        return (this.stack[this.stkArr.length - 1]);
+     }
+     isEmpty() {
+        return this.stack.length == 0;
+     }
+     size() {
+        return this.stack.length;
+    }
+     clear() {
+        document.write("The stack is cleared..!" + "<br>");
+        this.stkArr = [];
+     }
+  }
+
+  let baralho = new Baralho();
+  baralho.add(1);
+  baralho.add(2);
+  baralho.add(3);
+  baralho.add(4);
+  baralho.add(5);
+  console.log(baralho);
+
