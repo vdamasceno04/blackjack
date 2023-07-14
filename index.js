955af52809548e81b63996a34c1a7f54e5ef14d7
victordamascenooliveira@gmail.com <victordamascenooliveira@gmail.com>
2023-07-14 15:46:31 -0300

    api reads db info

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
+import userRoutes from "./routes/users.js";
 import cors from "cors";
 
-const app = express;
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/", userRoutes);
-app.listen(3306);
+app.listen(3300);
