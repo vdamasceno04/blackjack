a23ca05115f08f6f197eabc739ad92aad1426b97
vdamasceno04 <vdamasceno04@gmail.com>
2023-07-26 17:09:03 -0300

    fix routes manager

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,11 +3,9 @@ import { readUser, createUser, updateUser, deleteUser } from "../controllers/use
 
 const router = express.Router();
 
-//router.delete("/", deleteUser); //esse acesso nao funciona
-//router.post("/", createUser); //esse acesso nao funciona
-//router.put("/", updateUser); //esse acesso nao funciona
-//por algum motivo, o acesso router.get funciona, e todas as funções do 
-//crud definidas no user.js tambem funcionam se forem passadas por parâmetro no router.get
-router.get("/", readUser);
+router.delete("/:id", deleteUser); //D
+router.put("/:id", updateUser); //U
+router.post("/", createUser); //C
+router.get("/", readUser); //R
 
 export default router;
